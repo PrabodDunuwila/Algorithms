70c780155550b620af512da4b6c76ce158cee0a3
Prabod Dunuwila <praboddunuwila@gmail.com>
2020-05-24 20:04:08 +0530

    generic algorithm

--- water-jug-problem/water-jug-algo.py
+++ water-jug-problem/water-jug-algo.py
@@ -4,54 +4,27 @@ Created on Wed May 20 19:47:24 2020
 """
 
 def fill_v1(v1, v2, v1_max):
-    while v1 < v1_max:
-        if v2 == 0:
-            break
+    while v1 < v1_max and v2 != 0:
         v2 -= 1
         v1 += 1
-    return (v1, v2)
+    return v1, v2
 
-def fill_three_litre_jug():
-    v1, v1_max = 0, 4
-    v2, v2_max = 0, 3
-    print("Maximum volume of v1: {}L, v2: {}L".format(v1_max, v2_max))
-    print("Volume at different stages : \n(v1, v2) ")
-    while v1 != 2:
-        #Fill 3 liter jug fully.
+def fill_jug_algo(v1_max, v2_max, value):
+    v1, v1_max = 0, v1_max
+    v2, v2_max = 0, v2_max
+    print("\nVolume at different stages : \n{}L jar  |  {}L jar ".format(v1_max, v2_max))
+    print("------------------")
+    print("    {}   |   {}".format(v1, v2))
+    while v1 != value and v2 != value:
         v2 = v2_max
-        print((v1, v2))
-        #Fill 3 liter jug using 4 liter jug.
-        v1, v2 = fill_v1(v1, v2, v1_max)
-        if v1 == 2 or v2 == 2:
-            break
-        print((v1, v2))
-    print((v1, v2))
-
-def fill_four_litre_jug():
-    v1, v1_max = 0, 3
-    v2, v2_max = 0, 4   
-    print("Maximum volume of v1: {}L, v2: {}L".format(v1_max, v2_max))
-    print("Volume at different stages : \n(v1, v2) ")
-    while v2 != 2:
-        #Fill 4 liter jug fully.
-        v2 = v2_max
-        print((v1, v2))
-        #Fill 3 liter jug using 4 liter jug.
-        v1, v2 = fill_v1(v1, v2, v1_max)
-        if v1 == 2 or v2 == 2:
-            break
-        print((v1, v2))
-        #Empty 3 liter jug
-        v1 = 0
-        print((v1, v2))
-        #Fill 3 liter jug using 4 liter jug.
-        v1, v2 = fill_v1(v1, v2, v1_max)
-        print((v1, v2))
-    print((v1, v2))
+        print("    {}   |   {}".format(v1, v2))
+        while v2 != 0 and v1 != value and v2 != value:
+            if v1 == v1_max and v2 != 0:
+                v1 = 0
+                print("    {}   |   {}".format(v1, v2))
+            v1, v2 = fill_v1(v1, v2, v1_max)
+            print("    {}   |   {}".format(v1, v2))
     
 if __name__ == "__main__":
-    print("Fill 3L jug with 2L")
-    fill_three_litre_jug()
-    print("-------------------------")
-    print("Fill 4L jug with 2L")
-    fill_four_litre_jug()
+    fill_jug_algo(v1_max = 3, v2_max = 4, value = 2)
+    fill_jug_algo(v1_max = 4, v2_max = 3, value = 2)
