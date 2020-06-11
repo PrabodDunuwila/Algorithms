59bc5dab9c3ba29d0e768f5df7cdbbf10708e8f7
Prabod Dunuwila <praboddunuwila@gmail.com>
2020-06-11 16:58:30 +0530

    Add readme for TSP

--- Travelling Salesman Problem/Naive.py
+++ Travelling Salesman Problem/Naive.py
@@ -13,65 +13,60 @@ def create_matrix(number_of_cities):
         for y in range(0, cols):
             if x == y:
                 break
-            matrix[x,y] = matrix[y,x] = random.randint(1, 100)
+            matrix[x, y] = matrix[y, x] = random.randint(1, 100)
     print("Matrix : ")
     print(matrix)
     return matrix
-        
 
-def permutation(lst): 
-    if len(lst) == 0: 
-        return [] 
-    if len(lst) == 1: 
-        return [lst] 
+
+def permutation(lst):
+    if len(lst) == 0:
+        return []
+    if len(lst) == 1:
+        return [lst]
 
     l = []
-  
+
     for i in range(len(lst)):
-       m = lst[i] 
-       remLst = lst[:i] + lst[i+1:] 
-       for p in permutation(remLst): 
-           l.append([m] + p)
-           
+        m = lst[i]
+        remLst = lst[:i] + lst[i + 1:]
+        for p in permutation(remLst):
+            l.append([m] + p)
+
     return l
 
 
 def algorithm(number_of_cities):
     matrix = create_matrix(number_of_cities)
     city_lst = [*range(1, number_of_cities, 1)]
-    
+
     city_permutations = permutation(city_lst)
-    
+
     for p in city_permutations:
         p.insert(0, 0)
         p.append(0)
-        '''if len(p) > 3:
-            for p in city_permutations:
-                new_lst = p[::-1]
-                if new_lst in city_permutations:
-                    city_permutations.remove(new_lst)'''
     print(city_permutations)
     min_dist = sys.maxsize
     optimal_perm = []
-    
-    #Find optimal solution
+
+    # Find optimal solution
     for perm in city_permutations:
         dist = 0
         for i in range(0, number_of_cities):
-            if i != number_of_cities-1:
-                dist += matrix[perm[i],perm[i+1]]
+            if i != number_of_cities - 1:
+                dist += matrix[perm[i], perm[i + 1]]
             else:
-                dist += matrix[perm[i],perm[0]]
+                dist += matrix[perm[i], perm[0]]
         if dist < min_dist:
             min_dist = dist
             optimal_perm = perm
     print("Minimum distance : {}, Optimal route : {}".format(min_dist, optimal_perm))
     return optimal_perm
-    
-    
+
+
 def calculate_time(test_cases):
     exec_time = []
-    for i in [*range(2, test_cases+1, 1)]:
+    for i in [*range(2, test_cases + 1, 1)]:
         start = time.time()
         algorithm(i)
         execution_time = time.time() - start
@@ -82,13 +77,13 @@ def calculate_time(test_cases):
 
 
 def plot_graph(test_cases, exec_time):
-    x = [*range(2, test_cases+1, 1)]
+    x = [*range(2, test_cases + 1, 1)]
     plt.plot(x, exec_time)
     plt.xlabel('number of cities')
     plt.ylabel('execution time(s)')
     plt.title('TSP algorithm execution')
     plt.show()
 
+
 if __name__ == "__main__":
     calculate_time(10)
-
--- Travelling Salesman Problem/README.md
+++ Travelling Salesman Problem/README.md
@@ -1,25 +1,37 @@
 # TSP
 
+<pre>
 Implementations for Travelling Salesman Problem using,
-	1. mlrose library
-	2. implement algorithm from scratch using naive method.
-	3. using depth first search
-	4. using breadth first search
-	5. using uniform cost search
+    1. using mlrose library
+    2. using naive method
+    3. using Depth First Search and Breadth First Search
+    4. using Uniform Cost Search
+</pre>
 
-Implement algorithm from scratch using naive method
+<pre>
+1. Using mlrose library
+    - Provide a list of coordinates and get a best state.
+</pre>
 
-The algorithm used to solve the TSP has the following steps.
-	1. Create a matrix with the distances for each city using random values.
-	2. Generate all the permutations of the routes between the cities to start travelling from ‘city zero’ and visit all other cities exactly once before returning to the ‘city zero’ back. The permutations include only unique routes.
-	3. Then find the total distance for each route.
-	4. Find the minimum distance and its route.
+<pre>
+2. Naive method
+    - Create a matrix with the distances for each city using random values.
+    - Generate all the permutations of the routes between the cities to start travelling from ‘city zero’ and visit all other cities exactly once before returning to the ‘city zero’ back. The permutations include only unique routes.
+    - Then find the total distance for each route.
+    - Find the minimum distance and its route.
+</pre>
 
-Console output produces a log  for the following values (Figure 1)
-	1. Matrix of distance
-	2. Minimum distance / Optimal route
-	3. Execution time
+<pre>
+3. Depth First Search and Breadth First Search
+    - Request user to input the matrix size
+    - Generate a matrix with random cost values.
+    - Request user to whether to use DFS or BFS to solve the problem
+    - Output the optimal path
+</pre>
 
-The execution time against the number of cities was plotted for the algorithm used for the calculation of TSP.
-
-
+<pre>
+4. Uniform Cost Search algorithm
+    - Request user to input the matrix size
+    - Generate a matrix with random cost values.
+    - Run the algorithm and print out the UCS path with the cost.
+</pre>
